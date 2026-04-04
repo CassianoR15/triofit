e708cfe94efee1f6a7f679e128843e43a7234fd0
CassianoR15 <159847621+CassianoR15@users.noreply.github.com>
2026-04-04 20:44:17 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// build: 2026-04-04
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
-  }
 })
